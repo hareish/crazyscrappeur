7b6058e526b6183a011f962d5b12aa019f561053
Hareish <64590529+hareish@users.noreply.github.com>
2021-04-22 21:20:02 +0200

    Delete spec directory

--- spec/dark_tradeur_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative "../lib/dark_tradeur"
-
-describe "Vérifier le nombre de résultats" do
-  it "au minimum 10" do
-    expect(get_currencies().count).to be > 10
-  end
-end
--- spec/merry_christmas.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../lib/merry_christmas'
-
-describe "Vérifier le nombre de résultats" do
-  it "au minimum 10" do
-    expect(connect().length).to be > 10
-  end
-end
